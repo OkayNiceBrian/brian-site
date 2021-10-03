1d38b073392f3e7c94c2accb0f6be007e41da4ad
Brian Jackson <brianjackson1224@gmail.com>
2021-10-03 10:52:24 -0700

    Functional weather page (unstyled)

--- resources/views/weather/weather.blade.php
+++ resources/views/weather/weather.blade.php
@@ -35,17 +35,28 @@
             ]);
 
             if ($response->getReasonPhrase() == 'Bad Request') {
-                echo "Invalid Zip Code.";
+                echo "<h3>Invalid Zip Code.</h3>";
             } else {
                 $weatherData = $response->json();
-                echo $weatherData["location"]["name"];
             }
         }
     ?>
 
     @isset($weatherData)
+        <?php 
+            $cityState = $weatherData["location"]["name"] . ", " . $weatherData["location"]["region"] . ", " . $weatherData["location"]["country"];
+            list($date, $time) = explode(" ", $weatherData["location"]["localtime"]);
+            list($year, $month, $day) = explode("-", $date);
+            $temperatureF = $weatherData["current"]["temp_f"] . " ℉";
+            $windMph = "Wind: " . $weatherData["current"]["wind_mph"] . " m/h " . $weatherData["current"]["wind_dir"];
+        ?>
         <div>
-            <h3></h3>
+            <h3><?php echo $cityState; ?></h3>
+            <h3><?php echo $month . "/" . $day . "/" . $year; ?></h3>
+            <h3><?php echo $time; ?></h3>
+            <img src="<?php echo $weatherData["current"]["condition"]["icon"] ?>" alt="Weather Icon">
+            <p><?php echo $temperatureF . " " . $weatherData["current"]["condition"]["text"]; ?></p>
+            <p><?php echo $windMph; ?></p>
         </div>
     @endisset
 
