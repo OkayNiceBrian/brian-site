c5633894893c15387cdc5e539ab5e256a65dc845
Brian Jackson <brianjackson1224@gmail.com>
2021-10-03 16:33:57 -0700

    Styled weather page

--- resources/views/weather/weather.blade.php
+++ resources/views/weather/weather.blade.php
@@ -3,64 +3,123 @@
 @extends('layouts._layout')
 
 @section('title',  'Weather')
-@section('about')
-
+@section('head')
+    <style>
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;
+            padding: 0;
+        }
+        .dataContainer {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin: 0;
+            padding: 0;
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+            width: 450;
+            background-color: lavender;
+            border-width: 20;
+            border-color: cadetblue;
+        }
+        .dateTime {
+            display: flex;
+            flex-direction: row;
+        }
+        .iconAndTemp {
+            display: flex;
+            flex-direction: row-reverse;
+            justify-content: center;
+            padding-top: 0;
+            margin-top: 0;
+        }
+        .iconAndText {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding-right: 40;
+            color: darkgrey;
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        }
+        .titleText {
+            font-size: 24pt;
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        }
+        .zipForm {
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+            padding-top: 20;
+        }
+    </style>
 @stop()
 
 @section('navbar')
 
 @section('content')
-    
-    <form action="" method="get">
-        @csrf
-        <label for="zip">Enter Zip Code:</label>
-        <input type="number" id="zip" name="zip">
-        <input type="submit" value="See Weather">
-    </form>
 
-    <?php
-        use Illuminate\Support\Facades\Http;
+    <div class="container">
 
-        $weatherData;
+        <h1 class="titleText">Current Weather</h1>
 
-        if (isset($_GET["zip"])) {
-            $zip = $_GET["zip"];
+        <?php
+            use Illuminate\Support\Facades\Http;
 
-            $request = 'https://weatherapi-com.p.rapidapi.com/current.json';
-            $response = Http::withHeaders([
-                'x-rapidapi-host' => 'weatherapi-com.p.rapidapi.com',
-                'x-rapidapi-key' => env('RAPID_API_KEY'),
-            ])->get($request, [
-                'q' => $zip,
-            ]);
+            $weatherData;
 
-            if ($response->getReasonPhrase() == 'Bad Request') {
-                echo "<h3>Invalid Zip Code.</h3>";
-            } else {
-                $weatherData = $response->json();
+            if (isset($_GET["zip"])) {
+                $zip = $_GET["zip"];
+
+                $request = 'https://weatherapi-com.p.rapidapi.com/current.json';
+                $response = Http::withHeaders([
+                    'x-rapidapi-host' => 'weatherapi-com.p.rapidapi.com',
+                    'x-rapidapi-key' => env('RAPID_API_KEY'),
+                ])->get($request, [
+                    'q' => $zip,
+                ]);
+
+                if ($response->getReasonPhrase() == 'Bad Request') {
+                    echo "<h3>Invalid Zip Code.</h3>";
+                } else {
+                    $weatherData = $response->json();
+                }
             }
-        }
-    ?>
-
-    @isset($weatherData)
-        <?php 
-            $cityState = $weatherData["location"]["name"] . ", " . $weatherData["location"]["region"] . ", " . $weatherData["location"]["country"];
-            list($date, $time) = explode(" ", $weatherData["location"]["localtime"]);
-            list($year, $month, $day) = explode("-", $date);
-            $temperatureF = $weatherData["current"]["temp_f"] . " ℉";
-            $windMph = "Wind: " . $weatherData["current"]["wind_mph"] . " m/h " . $weatherData["current"]["wind_dir"];
         ?>
-        <div>
-            <h3><?php echo $cityState; ?></h3>
-            <h3><?php echo $month . "/" . $day . "/" . $year; ?></h3>
-            <h3><?php echo $time; ?></h3>
-            <img src="<?php echo $weatherData["current"]["condition"]["icon"] ?>" alt="Weather Icon">
-            <p><?php echo $temperatureF . " " . $weatherData["current"]["condition"]["text"]; ?></p>
-            <p><?php echo $windMph; ?></p>
-        </div>
-    @endisset
 
-    <div>
+        @isset($weatherData)
+            <?php 
+                $cityState = $weatherData["location"]["name"] . ", " . $weatherData["location"]["region"] . ", " . $weatherData["location"]["country"];
+                list($date, $time) = explode(" ", $weatherData["location"]["localtime"]);
+                list($year, $month, $day) = explode("-", $date);
+                $temperatureF = $weatherData["current"]["temp_f"] . " ℉";
+                $windMph = "Wind: " . $weatherData["current"]["wind_mph"] . " m/h ";
+            ?>
+            <div class="dataContainer">
+                <h3 style="margin-bottom: 0"><?php echo $cityState; ?></h3>
+                <div class="dateTime">
+                    <h4 style="padding-right: 15"><?php echo $month . "/" . $day . "/" . $year; ?></h3>
+                    <h4><?php echo $time; ?></h3>
+                </div>
+                <div class="iconAndTemp">
+                    <p style="font-size: 30pt; margin-top: 12;"><?php echo $temperatureF; ?></h1>
+                    <div class="iconAndText">
+                        <img src="<?php echo $weatherData["current"]["condition"]["icon"] ?>" alt="Weather Icon">
+                        <p style="margin-top: 0"><?php echo $weatherData["current"]["condition"]["text"]; ?></p>
+                    </div>
+                </div>
+                <p style="margin-top: 5;"><?php echo $windMph; ?></p>
+            </div>
+        @endisset
+    
+        <form class="zipForm" action="" method="get">
+            @csrf
+            <label for="zip">Enter Zip Code:</label>
+            <input type="number" id="zip" name="zip">
+            <input type="submit" value="See Weather">
+        </form>
+
     </div>
 
 @stop
