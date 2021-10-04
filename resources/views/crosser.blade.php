f9c79aacafdb74db386671a47531105882791852
Brian Jackson <brianjackson1224@gmail.com>
2021-10-04 09:38:47 -0700

    Centered crosser

--- resources/views/crosser.blade.php
+++ resources/views/crosser.blade.php
@@ -2,14 +2,26 @@
 
 @section('title',  'Crosser')
 @section('head')
-
+    <style>
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .container p {
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+            font-size: 30pt;
+            margin-top: 0;
+        }
+    </style>
 @stop
 
 @section('navbar')
 
 @section('content')
 
-    <div>
+    <div class="container">
+        <p>Crosser</p>
         <iframe src="https://preview.p5js.org/OkayNiceBrian/embed/P3dDM8PnN" height ="450" width="450"></iframe>
     </div>
 
