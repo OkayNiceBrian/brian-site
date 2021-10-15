5c108af1cc4a812575c3f1445e06cbbb7f0e106e
Brian Jackson <brianjackson1224@gmail.com>
2021-10-14 21:01:30 -0700

    Starting go

--- resources/views/go.blade.php
+++ resources/views/go.blade.php
@@ -2,10 +2,8 @@
 
 @section('title',  'Go')
 @section('head')
-    <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
 @stop()
 
 @section('navbar')
-
 @section('content')
 @stop()
