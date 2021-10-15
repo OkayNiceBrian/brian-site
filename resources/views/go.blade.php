3c488911a9855a5a3ccc44d05a8af236ffa23cc7
Brian Jackson <brianjackson1224@gmail.com>
2021-10-15 10:20:24 -0700

    We're placing stones!!

--- resources/views/go.blade.php
+++ resources/views/go.blade.php
@@ -68,6 +68,54 @@
             opacity: 0.3;
         }
     </style>
+    <script>
+        const stone = {
+            none: 0,
+            black: 1,
+            white: 2
+        };
+        let blackTurn = true;
+        let board = [
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none],
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none],
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none],
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none],
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none],
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none],
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none],
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none],
+            [stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none, stone.none]
+        ];
+
+        function stoneClick(id) {
+            const pointId = id.split(",");
+            const point = {
+                x: pointId[0],
+                y: pointId[1]
+            };
+
+            if (board[point.x][point.y] == stone.none) {
+                if (blackTurn) {
+                    setBlackStone(id, point.x, point.y);
+                } else {
+                    setWhiteStone(id, point.x, point.y);
+                }
+                blackTurn = !blackTurn;
+            }
+        }
+
+        function setBlackStone(id, x, y) {
+            board[x][y] = stone.black;
+            document.getElementById(id).classList.remove("empty");
+            document.getElementById(id).classList.add("black-piece");
+        }
+
+        function setWhiteStone(id, x, y) {
+            board[x][y] = stone.white;
+            document.getElementById(id).classList.remove("empty");
+            document.getElementById(id).classList.add("white-piece");
+        }
+    </script>
 @stop()
 
 @section('navbar')
@@ -75,103 +123,103 @@
     <div class="board">
         <table class="interactive-board">
             <tr>
-                <td><p class="black-piece"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,0" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,0" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,0" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,0" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,0" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,0" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,0" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,0" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,0" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
             <tr>
-                <td><p class="white-piece"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,1" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,1" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,1" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,1" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,1" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,1" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,1" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,1" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,1" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
             <tr>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,2" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,2" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,2" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,2" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,2" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,2" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,2" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,2" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,2" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
             <tr>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,3" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,3" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,3" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,3" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,3" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,3" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,3" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,3" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,3" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
             <tr>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,4" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,4" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,4" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,4" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,4" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,4" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,4" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,4" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,4" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
             <tr>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,5" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,5" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,5" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,5" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,5" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,5" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,5" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,5" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,5" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
             <tr>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,6" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,6" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,6" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,6" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,6" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,6" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,6" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,6" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,6" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
             <tr>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,7" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,7" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,7" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,7" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,7" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,7" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,7" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,7" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,7" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
             <tr>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
-                <td><p class="empty"></p></td>
+                <td><p id="0,8" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="1,8" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="2,8" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="3,8" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="4,8" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="5,8" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="6,8" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="7,8" class="empty" onclick="stoneClick(this.id)"></p></td>
+                <td><p id="8,8" class="empty" onclick="stoneClick(this.id)"></p></td>
             </tr>
         </table>
         <table class="visual-board">
